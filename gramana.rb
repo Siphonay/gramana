cfbed930a88ccda27492cac32f0a1086fed65ad5
Alexis Viguié <alexis.viguie@epitech.eu>
2016-09-18 01:17:44 +0000

    fixed typo

--- gramana.rb
+++ gramana.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # coding: utf-8
-# gramanaa: a Telegram anagram bot
+# gramana: a Telegram anagram bot
 # Written in Ruby by Alexis « Sam » « Siphoné » Viguié on the 18-09-2016
 # No license applied
 
