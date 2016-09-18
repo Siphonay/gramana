a2e05266338d8c1b46a6caa7da38ac657c563f5c
Alexis Viguié <alexis.viguie@epitech.eu>
2016-09-18 02:39:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gramana
 a Telegram anagram bot
+
+usage: `./gramana.rb [telegram bot api token]`
---
+++ gramana.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+# granama: a Telegram anagram bot
+# Written in Ruby by Alexis « Sam » « Siphoné » Viguié on the 18-09-2016
+# No license applied
+
+# Loading the required gem to make use of Telegram's bot API
+require 'telegram_bot'
+
+# Exiting the program if no argument is specified
+abort "please specify a telegram bot api token in argument." unless ARGV[0]
+
+# Use the token passed in argument
+gramana = TelegramBot.new(token: ARGV[0])
+
+# Processing every message the bot recieves
+gramana.get_updates(fail_silently: true) do |message|
+  puts "got message: #{message.text}"                           # Display in stdout the latest message recieved
+  word = message.text.split(" ")[0]                             # Get the message's first word to search its anagrams
+
+  # Building the reply
+  message.reply do |reply|
+    reply_anagrams = `an -w -m #{word.length} #{word}`                  # Calls the "an" command to find anagrams of words of the same lenght as the argument word
+                     .split("\n")                                       # Split the command output in order to process it as an array
+                     .map { |anagram| anagram.downcase }                # Lowercase all found anagrams
+                     .uniq                                              # Remove duplicates
+                     .delete_if { |anagram| anagram == word }           # If present, delete the original word from the results
+
+    # Building the string                 
+    reply.text = if reply_anagrams.size != 0                            # If the anagram list isn't empty,
+                   "anagrams for #{word}: #{reply_anagrams.join(" ")}"  # add them all to the message as words separated by spaces.
+                 else                                                   # If it is,
+                   "no anagrams found."                                 # report it.
+                 end
+
+    puts "sending message: #{reply.text}"                               # Display in stdout the next message that will be sent
+    reply.send_with(gramana)                                            # Sending the reply
+  end
+end
+
+# Sending an error code and message if the message loop is exited somehow
+abort "this should not have happened."
